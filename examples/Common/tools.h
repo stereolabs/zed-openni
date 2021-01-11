35533b42c4278ea142615276b8d419835dff223e
Walter Lucetti <walter.lucetti@stereolabs.com>
2021-01-11 22:28:39 +0100

    Add Stereolabs disclaimer

--- examples/Common/tools.h
+++ examples/Common/tools.h
@@ -18,66 +18,20 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef COLORVIZ_HPP
-#define COLORVIZ_HPP
+#ifndef TOOLS_HPP
+#define TOOLS_HPP
 
 #include <OpenNI.h>
-#include <opencv2/opencv.hpp>
 
-class ColorViz
-{
-private:
-    // Device
-    openni::Device device;
-    openni::VideoStream color_stream;
+#include <sstream>
+#include <stdexcept>
 
-    // Color Buffer
-    openni::VideoFrameRef color_frame;
-    cv::Mat color_mat;
-    uint32_t color_width = 640;
-    uint32_t color_height = 480;
-    uint32_t color_fps = 30;
+// Error Check Macro
+#define OPENNI_CHECK( ret )                                        \
+    if( ret != openni::STATUS_OK ){                               \
+        std::stringstream ss;                                     \
+        ss << "failed " #ret " " << std::hex << ret << std::endl; \
+        throw std::runtime_error( ss.str().c_str() );             \
+    }
 
-public:
-    // Constructor
-    ColorViz();
-
-    // Destructor
-    ~ColorViz();
-
-    // Processing
-    void run();
-
-private:
-    // Initialize
-    void initialize();
-
-    // Initialize Device
-    inline void initializeDevice();
-
-    // Initialize Color
-    inline void initializeColor();
-
-    // Finalize
-    void finalize();
-
-    // Update Data
-    void update();
-
-    // Update Color
-    inline void updateColor();
-
-    // Draw Data
-    void draw();
-
-    // Draw Color
-    inline void drawColor();
-
-    // Show Data
-    void show();
-
-    // Show Color
-    inline void showColor();
-};
-
-#endif // COLORVIZ_HPP
+#endif
